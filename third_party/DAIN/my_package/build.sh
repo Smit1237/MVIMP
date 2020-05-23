4d6ad56a7f071b42d019429222b2d1b7305d8781
Smit <smit1237@gmail.com>
2020-05-23 23:07:38 +0300

    Now really add build.sh mdifications

--- third_party/DAIN/my_package/build.sh
+++ third_party/DAIN/my_package/build.sh
@@ -5,41 +5,41 @@ source activate pytorch1.0.0
 
 cd MinDepthFlowProjection
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd FlowProjection
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd SeparableConv
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd InterpolationCh
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd DepthFlowProjection
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd Interpolation
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd SeparableConvFlow
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
 cd FilterInterpolation
 rm -rf build *.egg-info dist
-python setup.py install
+python3 setup.py install
 cd ..
 
